8bb64c35007f7572aaee32ad4fad3d9fa3610ea5
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-05-11 15:56:45 -0600

    env

--- scurvy-dogs.client/src/env.js
+++ scurvy-dogs.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev--ctam859.us.auth0.com'
+export const audience = 'https://ZacDev.com'
+export const clientId = 'YgqnjnhO43V4EqDthLJCutJJ8UKlzHHH'
--- scurvy-dogs/client/app/env.js
+++ scurvy-dogs/client/app/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'dev--ctam859.us.auth0.com'
+export const audience = 'https://ZacDev.com'
+export const clientId = 'YgqnjnhO43V4EqDthLJCutJJ8UKlzHHH'
