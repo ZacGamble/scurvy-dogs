44740ad11d991e221e90c628b4d53aae6f780400
ZacGamble <zacrgamble@gmail.com>
2022-05-12 11:21:17 -0600

    styling changes

--- scurvy-dogs.client/src/App.vue
+++ scurvy-dogs.client/src/App.vue
@@ -5,11 +5,7 @@
   <main>
     <router-view />
   </main>
-  <footer>
-    <div class="bg-dark text-light text-center p-4">
-      Made with 💖 by CodeWorks
-    </div>
-  </footer>
+  <footer></footer>
 </template>
 
 <script>
@@ -26,5 +22,4 @@ export default {
 </script>
 <style lang="scss">
 @import "./assets/scss/main.scss";
-
 </style>
--- scurvy-dogs.client/src/pages/HomePage.vue
+++ scurvy-dogs.client/src/pages/HomePage.vue
@@ -1,11 +1,8 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo" class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
+  <div class="container">
+    <div class="row">Boss</div>
+    <div class="row">Lobby information</div>
+    <div class="row">Player stats</div>
   </div>
 </template>
 
@@ -16,21 +13,4 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.home{
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-  .home-card{
-    width: 50vw;
-    > img{
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
-}
 </style>
