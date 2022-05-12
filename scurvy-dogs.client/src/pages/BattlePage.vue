e39e2b411b4a2e42fb88cdeedcb0ae58ab1c1084
TobyComon <toby.comon@gmail.com>
2022-05-12 15:25:42 -0600

    battle page, router changes

---
+++ scurvy-dogs.client/src/pages/BattlePage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="row bg-secondary justify-content-center">
+          <div class="col-md-5">
+            <img
+              class="img-fluid"
+              src="https://thiscatdoesnotexist.com"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <img class="ship-img" src="https://thiscatdoesnotexist.com" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.ship-img {
+  border-radius: 50%;
+}
+</style>
--- scurvy-dogs.client/src/router.js
+++ scurvy-dogs.client/src/router.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import { authGuard } from '@bcwdev/auth0provider-client'
+import { authGuard, authSettled } from '@bcwdev/auth0provider-client'
 
 function loadPage(page) {
   return () => import(`./pages/${page}.vue`)
@@ -9,12 +9,14 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: loadPage('HomePage')
+    component: loadPage('HomePage'),
+    beforeEnter: authSettled
   },
   {
     path: '/ships',
     name: 'Ships',
-    component: loadPage('ShipDetailsPage')
+    component: loadPage('ShipDetailsPage'),
+    beforeEnter: authGuard
   },
   {
     path: '/about',
@@ -25,6 +27,12 @@ const routes = [
     path: '/account',
     name: 'Account',
     component: loadPage('AccountPage'),
+    beforeEnter: authGuard
+  },
+  {
+    path: '/battle',
+    name: 'Battle',
+    component: loadPage('BattlePage'),
     beforeEnter: authGuard
   }
 ]
