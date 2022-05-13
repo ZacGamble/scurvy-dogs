761b840d53e931223c7784747b3d9fcf6ef42d58
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-05-13 11:45:22 -0600

    removed more values stuff

--- scurvy-dogs/client/app/main.js
+++ scurvy-dogs/client/app/main.js
@@ -1,9 +1,7 @@
 import { AuthController } from './Controllers/AuthController.js'
-import { ValuesController } from './Controllers/ValuesController.js'
 
 class App {
   authController = new AuthController();
-  valuesController = new ValuesController();
 }
 
 // @ts-ignore
