4f8772a50155964aa89b514236a12509a557a79d
AustinDye <thedyeguy1996@outlook.com>
2022-05-13 11:34:09 -0600

    styles

---
+++ scurvy-dogs.client/src/components/SideBar.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav class="sidebar px-3 text-dark">
+    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
+      <div class="d-flex text-dark">Ye Scurvy Dogs!</div>
+    </router-link>
+    <button
+      class="navbar-toggler text-dark"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarText"
+      aria-controls="navbarText"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto">
+        <li>
+          <router-link
+            :to="{ name: 'Ships' }"
+            class="btn selectable text-uppercase"
+          >
+            Ship Details
+          </router-link>
+        </li>
+      </ul>
+      <!-- LOGIN COMPONENT HERE -->
+      <Login />
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+a:hover {
+  text-decoration: none;
+}
+.nav-link {
+  text-transform: uppercase;
+}
+.navbar-nav .router-link-exact-active {
+  border-bottom: 2px solid var(--bs-success);
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+}
+.sidebar {
+  margin-top: 20%;
+  margin-left: 10%;
+}
+</style>
