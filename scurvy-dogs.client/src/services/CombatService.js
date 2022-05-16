73810df7cae4a57319900d69404ad53bbfa8543e
AustinDye <thedyeguy1996@outlook.com>
2022-05-16 13:50:47 -0600

    combat

--- scurvy-dogs.client/src/services/CombatService.js
+++ scurvy-dogs.client/src/services/CombatService.js
@@ -1,13 +1,21 @@
+import { AppState } from "../AppState"
+import Pop from "../utils/Pop"
 
 
 class CombatService { 
 
-  async attack(actorId, targetId){
-   
+  async attack(actor, target) {
+    target.health -= (actor.power - .02 * target.hull) 
   }
  
- async dodge(actorId, targetId) {
-  
+  async dodgeCalc(actor, target) {
+   const hit = flase
+   const speed = Math.floor(Math.random() * actor.speed)
+   const targetSpeed = Math.floor(Math.random() * target.speed)
+   if (speed <= targetSpeed) {
+     Pop.toast('MISS')
+   }
+   return hit = true
  }
 
  async damage(targetId) {
