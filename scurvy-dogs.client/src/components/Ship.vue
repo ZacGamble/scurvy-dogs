d1c1b79747f12ba045adac1be240204d106900b6
AustinDye <thedyeguy1996@outlook.com>
2022-05-22 09:30:32 -0600

    Create Ship.vue

---
+++ scurvy-dogs.client/src/components/Ship.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col-3">
+    <div class="user-ship">
+      <h5>{{ ship.name }}</h5>
+      <h6>{{ ship.durability }}</h6>
+      <img :src="ship.img" class="img-fluid" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    ship: {
+      type: Object,
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.user-ship {
+  outline: 1em black solid;
+}
+</style>
